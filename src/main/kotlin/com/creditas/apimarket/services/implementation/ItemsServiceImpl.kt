582cf333dd874176e6f7a681c8febff5b100e5dd
David Moreno <dmoreno788@gmail.com>
2019-11-04 16:37:29 +0100

    Implementado interface  IntemService en ItemsServiceImpl

--- src/main/kotlin/com/creditas/apimarket/services/implementation/ItemsServiceImpl.kt
+++ src/main/kotlin/com/creditas/apimarket/services/implementation/ItemsServiceImpl.kt
@@ -1,11 +1,17 @@
 package com.creditas.apimarket.services.implementation
 
+import com.creditas.apimarket.domain.dao.I_ItemsDao
 import com.creditas.apimarket.domain.entities.Item
 import com.creditas.apimarket.services.I_ItemService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
-class ItemsServiceImpl: I_ItemService {
+@Service
+class ItemsServiceImpl : I_ItemService {
+    @Autowired
+    private lateinit var  itemDao : I_ItemsDao
     override fun getItems(): List<Item> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return itemDao.findAll() as List<Item>
     }
 
 }
