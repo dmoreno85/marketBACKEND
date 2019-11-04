efa572a7dc1a8774076cb14a7d09310b290ac5b4
David Moreno <dmoreno788@gmail.com>
2019-11-04 16:45:03 +0100

    Implementado itemService en ItemsController

--- src/main/kotlin/com/creditas/apimarket/controllers/ItemsController.kt
+++ src/main/kotlin/com/creditas/apimarket/controllers/ItemsController.kt
@@ -1,5 +1,8 @@
 package com.creditas.apimarket.controllers
 
+import com.creditas.apimarket.domain.entities.Item
+import com.creditas.apimarket.services.I_ItemService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("api/v1")
 class ItemsController {
-    @RequestMapping("hola mundo")
-    fun HelloWorld():ResponseEntity<Unit>{
+    @Autowired
+    private lateinit var itemService: I_ItemService
+
+    @RequestMapping("hola")
+    fun HelloWorld(): ResponseEntity<Unit> {
         return ResponseEntity<Unit>(HttpStatus.OK)
     }
+
+    @RequestMapping("items", "GET", "application/json")
+            /*fun getPatients():ResponseEntity<List<Patient>>{
+                val list:List<Patient> = patientService.getPatients()
+                return ResponseEntity(list, HttpStatus.OK)
+            }*/
+    fun getPatients(): ResponseEntity<List<Item>> = ResponseEntity(itemService.getItems(), HttpStatus.OK)
 }
